e3959abf6a5ee8b66ebbb78d4cca8b9ef4d9288c
anthonyc_36 <anthonychristy78@gmail.com>
2019-12-09 07:15:09 -0500

    S11L128

---
+++ CITA-312-RPG-F19-ab4ea37efc5cd5d8bb9f4eda71259d69dcd746e5/Marcoux-CITA312-F19/Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)] // created a asset menu
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] ProgressionCharacterClass[] characterClasses = null;
+
+        [System.Serializable]
+        class ProgressionCharacterClass // creating a progression chart for all character classes
+        {
+            [SerializeField] CharacterClass characterClass; // each character class
+            [SerializeField] float[] health; // health
+        }
+    }
+}
