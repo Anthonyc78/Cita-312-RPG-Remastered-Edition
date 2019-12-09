88c2fd3044d36403efe9ae98cad463b439b67aca
anthonyc_36 <anthonychristy78@gmail.com>
2019-12-09 06:36:44 -0500

    S10L126

--- CITA-312-RPG-F19-ab4ea37efc5cd5d8bb9f4eda71259d69dcd746e5/Marcoux-CITA312-F19/Assets/Scripts/Combat/WeaponPickup.cs
+++ CITA-312-RPG-F19-ab4ea37efc5cd5d8bb9f4eda71259d69dcd746e5/Marcoux-CITA312-F19/Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,31 @@ namespace RPG.Combat
 {
     public class WeaponPickup : MonoBehaviour
     {
-        [SerializeField] Weapon weapon = null;
+        [SerializeField] Weapon weapon = null; // the weapon can now be called by name using this
+        [SerializeField] float respawnTime = 5; // pickup respawn timer
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player") // if a game object with the tag "Player" collides with the pickup 
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon); // than equip the weapon
-                Destroy(gameObject); // destroy the pickup object
+                StartCoroutine(HideForSeconds(respawnTime)); // call the couroutine to start respawn timer
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds) // respawn coroutine
+        {
+            ShowPickup(false); // disables the collider and the child
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true); // enables the collider and the child
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow); // disables and enables the collider and child to have the pickups disappear for 5 seconds and then reappear
             }
         }
     }
